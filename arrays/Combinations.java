d144db65199b2766dffe71b187425abc451df0d3
pccper <zxqiu90@gmail.com>
2017-02-13 00:20:05 -0800

    combinations

--- arrays/Combinations.java
+++ arrays/Combinations.java
@@ -16,6 +16,8 @@ If n = 4 and k = 2, a solution is:
 解：
 DFS
 
+方法一：
+
 用path记录DFS的路径。每遇到一个数字就将这个数字放进path，然后从下一个数字开始下一次递归。
 当path中已经有k个数字，这时将path存入结果list。
 
@@ -48,5 +50,48 @@ public class Solution {
             helper(n, i + 1, k, ret, path);
             path.remove(path.size() - 1);
         }
+    }
+}
+
+
+/*
+
+方法二：
+
+DFS思路基本一致，换个写法。
+自底向上生成结果数组。
+需要直到当前处于第几层递归，当递归层数与k一致时，对每一个数生成list。每一层拿到下一层的返回值时，将当前值加进每一个list中。直到返回到顶层方法。
+
+*/
+
+
+public class Solution {
+    /**
+     * @param n: Given the range of numbers
+     * @param k: Given the numbers of combinations
+     * @return: All the combinations of k numbers out of 1..n
+     */
+    public List<List<Integer>> combine(int n, int k) {
+		return combineHelper(n, 1, k, 1);
+    }
+    
+    private List<List<Integer>> combineHelper(int n, int start, int k, int lvl) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        
+        for (int i = start; i <= n; i++) {
+            if (lvl == k) {
+                List<Integer> list = new ArrayList<Integer>();
+                list.add(i);
+                ret.add(list);
+            } else {
+                List<List<Integer>> list = combineHelper(n, i + 1, k, lvl + 1);
+                for (List<Integer> l : list) {
+                    l.add(0, i);
+                    ret.add(l);
+                }
+            }
+        }
+        
+        return ret;
     }
 }
