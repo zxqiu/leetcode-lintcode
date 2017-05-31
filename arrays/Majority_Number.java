f1069ea285a057335c54cb84848b3933d09be9e5
zxqiu <Naisrep007>
2017-05-30 23:45:16 -0700

    Majority Number

--- arrays/Majority_Number.java
+++ arrays/Majority_Number.java
@@ -18,12 +18,12 @@ O(n) time and O(1) extra space
 解：
 用candidate来保存数字，并用cnt来统计该数字的数量。
 当遇到一个跟candidate相同的数字时，cnt加一，否则减一。
-当cnt等于0时，更新candidate为当前的数字，同时cnt变为1，表示该数字有一个。
+当cnt等于0时，更新candidate为当前的数字，同时cnt不变。
 输出最后一个candidate
 
-比如输入为：	1，1，1，2，2，2，   1
-cnt：		1，2，3，2，1，0->1，0->1
-candidate：	1，1，1，1，1，1->2，2->1
+比如输入为：	1，1，1，2，2，2，2，1，1
+cnt：		1，2，3，2，1，0，0，0，1
+candidate：	1，1，1，1，1，1，2，1，1
 
 输入：		1，1，1，1，2，2，2
 cnt：		1，2，3，4，3，2，1
@@ -37,15 +37,14 @@ public class Solution {
      * @return: find a  majority number
      */
     public int majorityNumber(ArrayList<Integer> nums) {
-        int candidate = 0, cnt = 0;
+        int candidate, cnt = 0;
+        candidate = nums.get(0);
         
         for (int i = 0; i < nums.size(); i++) {
-            if (cnt == 0) {
-                candidate = nums.get(i);
-            }
-            
             if (candidate == nums.get(i)) {
                 cnt++;
+            } else if (cnt == 0) {
+                candidate = nums.get(i);
             } else {
                 cnt--;
             }
