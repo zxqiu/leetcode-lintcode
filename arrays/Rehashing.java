9e29e3eef6c1c240906d2c98976e7fe0b8a1e0c6
zxqiu <Naisrep007>
2017-06-14 23:07:12 -0700

    Rehashing

---
+++ arrays/Rehashing.java
@@ -0,0 +1,109 @@
+/*
+
+
+Rehashing
+
+
+
+The size of the hash table is not determinate at the very beginning. If the total size of keys is too large (e.g. size >= capacity / 10), we should double the size of the hash table and rehash every keys. Say you have a hash table looks like below:
+
+size=3, capacity=4
+
+[null, 21, 14, null]
+       ↓    ↓
+       9   null
+       ↓
+      null
+The hash function is:
+
+int hashcode(int key, int capacity) {
+    return key % capacity;
+}
+here we have three numbers, 9, 14 and 21, where 21 and 9 share the same position as they all have the same hashcode 1 (21 % 4 = 9 % 4 = 1). We store them in the hash table by linked list.
+
+rehashing this hash table, double the capacity, you will get:
+
+size=3, capacity=8
+
+index:   0    1    2    3     4    5    6   7
+hash : [null, 9, null, null, null, 21, 14, null]
+Given the original hash table, return the new hash table after rehashing .
+
+ Notice
+
+For negative integer in hash table, the position can be calculated as follow:
+
+C++/Java: if you directly calculate -4 % 3 you will get -1. You can use function: a % b = (a % b + b) % b to make it is a non negative integer.
+Python: you can directly use -1 % 3, you will get 2 automatically.
+
+Example
+Given [null, 21->9->null, 14->null, null],
+
+return [null, 9->null, null, null, null, 21->null, 14->null, null]
+
+
+解：
+遍历每个结点，计算新的hash值然后放进新的表中即可。
+需要注意的是每个结点需要先出队，然后再插入新表。
+
+
+*/
+
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *      int val;
+ *      ListNode next;
+ *      ListNode(int x) {
+ *          val = x;
+ *          next = null;
+ *      }
+ *  }
+ */
+public class Solution {
+    /**
+     * @param hashTable: A list of The first node of linked list
+     * @return: A list of The first node of linked list which have twice size
+     */    
+    public ListNode[] rehashing(ListNode[] hashTable) {
+        ListNode[] ret = new ListNode[hashTable.length * 2];
+        int size = ret.length;
+        
+        for (ListNode node : hashTable) {
+            ListNode dummy, iter;
+            
+            dummy = new ListNode(0);
+            dummy.next = node;
+            iter = dummy;
+            
+            while (iter.next != null) {
+                addToTable(ret, hashFunc(iter.next, size), iter);
+            }
+        }
+        
+        return ret;
+    }
+    
+    private void addToTable(ListNode[] ret, int hash, ListNode node) {
+        ListNode tmp = node.next;
+        node.next = tmp.next;
+        tmp.next = null;
+        
+        if (ret[hash] == null) {
+            ret[hash] = tmp;
+        } else {
+            ListNode iter = ret[hash];
+            for (; iter.next != null; iter = iter.next);
+            iter.next = tmp;
+        }
+    }
+    
+    private int hashFunc(ListNode node, int size) {
+        if (node.val >= 0) {
+            return node.val % size;
+        } else {
+            return (node.val % size + size) % size;
+        }
+    }
+};
+
