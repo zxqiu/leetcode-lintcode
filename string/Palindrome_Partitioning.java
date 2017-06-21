96ca4c0fd24c6ff076015223cbdf81a6c4304f25
zxqiu <zxqiu90@gmail.com>
2017-06-20 21:49:56 -0700

    Update Palindrome_Partitioning.java

--- string/Palindrome_Partitioning.java
+++ string/Palindrome_Partitioning.java
@@ -20,6 +20,8 @@ Given s = "aab", return:
 
 
 解：
+
+方法一：
 DFS
 
 当输入字符串的前i个字符可以构成回文的时候，将从第i+1开始的字符串作为下一次递归的输入。
@@ -73,5 +75,53 @@ public class Solution {
         }
         
         return true;
+    }
+}
+
+/*
+
+方法二：
+先用Dynanmic Programming求解任意子字符串是否为回文，然后再用DFS求解partition。
+
+*/
+
+public class Solution {
+    /**
+     * @param s: A string
+     * @return: A list of lists of string
+     */
+    public List<List<String>> partition(String s) {
+        boolean[][] isP = new boolean[s.length()][s.length()];
+        List<List<String>> ret = new ArrayList<List<String>>();
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            isP[i][i] = true;
+            for (int j = i + 1; j < s.length(); j++) {
+                if ((j == i + 1 || isP[i + 1][j - 1]) && s.charAt(i) == s.charAt(j)) {
+                    isP[i][j] = true;
+                }
+            }
+        }
+        
+        helper(ret, new ArrayList<String>(), s, 0, isP);
+        
+        return ret;
+    }
+    
+    private void helper(List<List<String>> ret, List<String> path, String s, int start, boolean[][] isP) {
+        if (start == s.length()) {
+            ret.add(new ArrayList<String>(path));
+            return;
+        }
+        
+        for (int i = start; i < s.length(); i++) {
+            if (!isP[start][i]) {
+                continue;
+            }
+            
+            path.add(s.substring(start, i + 1));
+            helper(ret, path, s, i + 1, isP);
+            path.remove(path.size() - 1);
+        }
     }
 }
