e23c1ad708b5bd1e4f25dbf67d893a848f0608d1
zxqiu <zxqiu90@gmail.com>
2017-09-02 10:46:42 -0700

    Update and rename Forg_Jump.java to Frog_Jump.java

--- arrays/Frog_Jump.java
+++ arrays/Frog_Jump.java
@@ -1,6 +1,6 @@
 /*
 
-Forg Jump
+Frog Jump
 
 A frog is crossing a river. The river is divided into x units and at each unit there may or may not exist a stone. The frog can jump on a stone, but it must not jump into the water.
 
